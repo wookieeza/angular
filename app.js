41fcd6297c518536d26690104e1eb800559a0ed4
Debbie Wood <dwood@whatclinic.com>
2015-05-29 20:07:15 +0100

    form has reviewsController pushes review to product reviews array

--- app.js
+++ app.js
@@ -16,6 +16,15 @@
      };
   });
 
+  app.controller('ReviewController', function(){
+  	this.review = {};
+
+  	this.addReview = function(product) {
+  		product.reviews.push(this.review);
+  		this.review = {};
+  	}
+  })
+
 
 var gems = [
   {
